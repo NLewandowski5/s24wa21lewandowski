1f715d7db1372dbdbe75c5045b3ecde5ca77ada9
Nolan Lewandowski <122854016+NLewandowski5@users.noreply.github.com>
2024-03-20 18:39:38 -0500

    updated app.js
    updated app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var indexRouter = require('./routes/mydata');
+var mydataRouter = require('./routes/mydata');
 var usersRouter = require('./routes/users');
 
 var app = express();
@@ -16,13 +16,14 @@ app.set('view engine', 'pug');
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use('/mydata', mydataRouter);
+
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/mydata', mydataRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
